15b02cbcbca61f6ab3eb2e1b034ed803f4df47c5
Merkwurdichliebe <11777208+Merkwurdichliebe@users.noreply.github.com>
2021-07-17 18:43:46 +0200

    Cleanup

--- beacon2map.py
+++ beacon2map.py
@@ -16,17 +16,52 @@ __license__ = "GPL"
 __version__ = "1.0"
 
 import sys
-import pandas as pd
 import math
+
 from PySide6 import QtCore, QtWidgets
 from PySide6.QtGui import QColor, QFont, QPen, QBrush, QPolygon, QPainter
 from PySide6.QtCore import Qt, QRectF, QPoint
 
+from markerdata import MarkerData
 
 FILENAME = 'sub-sample.csv'
 
+MAJOR_GRID = 500
+MINOR_GRID = 100
 
-class MyWidget(QtWidgets.QWidget):
+MAJOR_GRID_COLOR = QColor('dodgerblue')
+MINOR_GRID_COLOR = QColor('mediumblue')
+BACKGROUND_COLOR = QColor('darkblue')
+MARKER_DONE_COLOR = QColor('slateblue')
+
+INIT_SCALE = 0.75
+WIN_WIDTH = 2560
+WIN_HEIGHT = 1440
+
+FONT_FAMILY = 'Helvetica'
+FONT_SIZE = 16
+FONT_BOLD = True
+
+MARKERS = {
+    'pod':         {'color': 'limegreen', 'shape': 'star'},
+    'wreck':       {'color': 'bisque', 'shape': 'x'},
+    'biome':       {'color': 'coral', 'shape': 'circle'},
+    'interest':    {'color': 'gold', 'shape': 'triangle'},
+    'alien':       {'color': 'orchid', 'shape': 'square'},
+    'mur':         {'color': 'deepskyblue', 'shape': 'square'},
+    'misc':        {'color': 'darkorange', 'shape': 'circle'}
+    }
+
+SHAPES = {
+    'square': [(-4, -4), (4, -4), (4, 4), (-4, 4)],
+    'triangle': [(0, -4), (4, 4), (-4, 4)],
+    'star': [(-1, -1), (0, -5), (1, -1), (5, 4), (0, 1), (-5, 4), (-1, -1)],
+    'x': [(0, -2), (3, -5), (5, -3), (2, 0), (5, 3),
+          (3, 5), (0, 2), (-3, 5), (-5, 3), (-2, 0), (-5, -3), (-3, -5)]
+    }
+
+
+class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
@@ -40,62 +75,48 @@ class MyWidget(QtWidgets.QWidget):
 class MapMarker(QtWidgets.QGraphicsItem):
     def __init__(self, marker, label, depth, done):
         super().__init__()
-        self.setFlag(QtWidgets.QGraphicsItem.ItemIsSelectable)
-        self.setFlag(QtWidgets.QGraphicsItem.ItemIgnoresTransformations)
         self.marker = marker
         self.label = label
         self.depth = depth
         self.done = done
-        self.icons = {
-            'pod':         {'color': 'limegreen', 'shape': 'star'},
-            'wreck':       {'color': 'bisque', 'shape': 'x'},
-            'biome':       {'color': 'coral', 'shape': 'circle'},
-            'interest':    {'color': 'gold', 'shape': 'triangle'},
-            'alien':       {'color': 'orchid', 'shape': 'square'},
-            'mur':         {'color': 'deepskyblue', 'shape': 'square'},
-            'misc':        {'color': 'darkorange', 'shape': 'circle'}
-            }
-        self.done_color = QColor('slateblue')
-        self.color = self.done_color if self.done else QColor(
-            self.icons[self.marker]['color'])
-        self.shapes = {
-            'square': self.build_polygon(
-                [(-4, -4), (4, -4), (4, 4), (-4, 4)]),
-            'triangle': self.build_polygon(
-                [(0, -4), (4, 4), (-4, 4)]),
-            'star': self.build_polygon(
-                [(-1, -1), (0, -5), (1, -1), (5, 4),
-                 (0, 1), (-5, 4), (-1, -1)]),
-            'x': self.build_polygon(
-                [(0, -2), (3, -5), (5, -3), (2, 0), (5, 3),
-                 (3, 5), (0, 2), (-3, 5), (-5, 3), (-2, 0), (-5, -3), (-3, -5)])
-        }
 
-    @staticmethod
-    def build_polygon(coordinates):
-        return QPolygon([QPoint(*point) for point in coordinates])
+        # Set Qt flags
+        self.setFlag(QtWidgets.QGraphicsItem.ItemIsSelectable)
+        self.setFlag(QtWidgets.QGraphicsItem.ItemIgnoresTransformations)
+
+        # Build QPolygon dictionary from SHAPES coordinates
+        self.shapes = {}
+        for k, v in SHAPES.items():
+            self.shapes[k] = QPolygon([QPoint(*point)for point in SHAPES[k]])
+
+        # Set marker color
+        self.color = MARKER_DONE_COLOR if self.done else QColor(
+            MARKERS[self.marker]['color'])
 
     def paint(self, painter, option, widget):
-        p = QPainter()
-        pen = QPen(self.color)
         brush = QBrush(Qt.SolidPattern)
         brush.setColor(self.color)
-        painter.setPen(pen)
+        painter.setPen(QPen(self.color))
         painter.setBrush(brush)
 
-        shape_name = self.icons[self.marker]['shape']
+        # Draw marker shape
+        shape_name = MARKERS[self.marker]['shape']
         if shape_name == 'circle':
             painter.drawEllipse(-4, -4, 8, 8)
         else:
             painter.drawPolygon(self.shapes[shape_name])
+
+        # Draw marker label
         font = QFont()
-        font.setFamily("Helvetica")
-        font.setPixelSize(16)
-        font.setBold(True)
+        font.setFamily(FONT_FAMILY)
+        font.setPixelSize(FONT_SIZE)
+        font.setBold(FONT_BOLD)
         painter.setFont(font)
         painter.drawText(10, -2, self.label)
-        font.setPixelSize(14)
-        font.setBold(False)
+        
+        # Draw marker depth
+        font.setPixelSize(FONT_SIZE * 0.85)
+        font.setBold(FONT_BOLD)
         painter.setFont(font)
         painter.drawText(10, 14, str(self.depth) + 'm')
 
@@ -112,52 +133,47 @@ class MapMarker(QtWidgets.QGraphicsItem):
 
 
 class MapScene(QtWidgets.QGraphicsScene):
+
     def __init__(self, filename):
         super().__init__()
 
-        # Draw the grid:
-
-        # Major and minor grid intervals
-        major_grid = 500
-        minor_grid = 100
-
         # Get the marker list
-        marker_list = MarkerList(filename)
+        marker_data = MarkerData(filename)
 
-        # Calculate the minimum and maximum values
-        # for the grid
-        x_min = math.floor(marker_list.extents_x[0]/major_grid) * major_grid
-        x_max = math.ceil(marker_list.extents_x[1]/major_grid) * major_grid
-        y_min = math.floor(marker_list.extents_y[0]/major_grid) * major_grid
-        y_max = math.ceil(marker_list.extents_y[1]/major_grid) * major_grid
+        # Calculate the minimum and maximum values for the grid
+        ext = marker_data.get_extents()
+        x_min = math.floor(ext[0][0]/MAJOR_GRID) * MAJOR_GRID
+        x_max = math.ceil(ext[0][1]/MAJOR_GRID) * MAJOR_GRID
+        y_min = math.floor(ext[1][0]/MAJOR_GRID) * MAJOR_GRID
+        y_max = math.ceil(ext[1][1]/MAJOR_GRID) * MAJOR_GRID
 
         # Draw minor grid
-        for x in range(x_min, x_max+1, minor_grid):
-            self.addLine(x, y_min, x, y_max, QColor('mediumblue'))
-        for y in range(y_min, y_max+1, minor_grid):
-            self.addLine(x_min, y, x_max, y, QColor('mediumblue'))
+        for x in range(x_min, x_max+1, MINOR_GRID):
+            self.addLine(x, y_min, x, y_max, MINOR_GRID_COLOR)
+        for y in range(y_min, y_max+1, MINOR_GRID):
+            self.addLine(x_min, y, x_max, y, MINOR_GRID_COLOR)
 
         # Draw major grid
-        for x in range(x_min, x_max+1, major_grid):
-            self.addLine(x, y_min, x, y_max, QColor('dodgerblue'))
-        for y in range(y_min, y_max+1, major_grid):
-            self.addLine(x_min, y, x_max, y, QColor('dodgerblue'))
+        for x in range(x_min, x_max+1, MAJOR_GRID):
+            self.addLine(x, y_min, x, y_max, MAJOR_GRID_COLOR)
+        for y in range(y_min, y_max+1, MAJOR_GRID):
+            self.addLine(x_min, y, x_max, y, MAJOR_GRID_COLOR)
 
         # Draw markers
-        for x, y, m, b, d, n in marker_list.processed:
+        for x, y, m, b, d, n in marker_data.get_markers():
             marker = MapMarker(m, b, d, True if n == 'x' else False)
             marker.setPos(x, y)
             self.addItem(marker)
 
 
 class MapView(QtWidgets.QGraphicsView):
-    def __init__(self, parent):
-        super(MapView, self).__init__(parent)
-        self.scale(0.75, 0.75)
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.scale(INIT_SCALE, INIT_SCALE)
         self._zoom = 0
         self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
         self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.setBackgroundBrush(QColor('darkblue'))
+        self.setBackgroundBrush(BACKGROUND_COLOR)
 
     def wheelEvent(self, event):
         factor = 0
@@ -171,48 +187,10 @@ class MapView(QtWidgets.QGraphicsView):
             self.scale(factor, factor)
 
 
-class MarkerList():
-    def __init__(self, filename):
-        # Constants
-        REF_DEPTH = 100             # Depth of the reference beacon
-
-        # Read CSV file
-        # Distance,Bearing,Depth,Name,Done,Color,Marker,Description
-        # 630,90,250,Foo,FALSE,red,o,Bar
-        df = pd.read_csv(filename)
-
-        # Calculate a horizontal distance to the reference point
-        delta = df['Distance']**2 - (df['Depth']-REF_DEPTH)**2
-        df['h'] = round(delta.apply(math.sqrt)).astype(int)
-
-        # Reverse the bearing (CSV holds the heading to the reference)
-        df['dir'] = (df['Bearing'] - 180) % 360
-
-        # Calculate the x, y coordinates from the distance and heading
-        df['x'] = (df['dir'].apply(
-            math.radians).apply(math.sin) * df['h']).astype(int)
-        # We inverse the y coordinates for Qt
-        df['y'] = (df['dir'].apply(
-            math.radians).apply(math.cos) * -1 * df['h']).astype(int)
-
-        # Convert everything to lists
-        x = df['x'].tolist()
-        y = df['y'].tolist()
-        marker = df['Marker'].tolist()
-        label = df['Name'].tolist()
-        depth = df['Depth'].tolist()
-        done = df['Done'].tolist()
-
-        self.processed = zip(x, y, marker, label, depth, done)
-
-        self.extents_x = (df['x'].min(), df['x'].max())
-        self.extents_y = (df['y'].min(), df['y'].max())
-
-
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
-    widget = MyWidget()
-    widget.resize(2560, 1440)
+    widget = MainWindow()
+    widget.resize(WIN_WIDTH, WIN_HEIGHT)
     widget.show()
 
     sys.exit(app.exec())
---
+++ markerdata.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+"""
+This module is a helper class for beacon2map.py
+
+It reads a CSV file of beacon markers and calculates x and y positions
+from bearing and distance data.
+"""
+
+__author__ = "Tal Zana"
+__copyright__ = "Copyright 2021"
+__license__ = "GPL"
+__version__ = "1.0"
+
+import pandas as pd
+import math
+
+REF_DEPTH = 100             # Depth of the reference beacon
+
+
+class MarkerData():
+    def __init__(self, filename):
+        # Read CSV file
+        # Distance,Bearing,Depth,Name,Marker,Done,Description
+        # e.g.:
+        # 988,355,458,Caves,interest,,"Cave network detected below"
+        # Lowercase 'x' is used to flag a marker as 'Done'
+        df = pd.read_csv(filename)
+
+        # Calculate a horizontal distance to the reference point
+        delta = df['Distance']**2 - (df['Depth']-REF_DEPTH)**2
+        df['h'] = round(delta.apply(math.sqrt)).astype(int)
+
+        # Reverse the bearing (CSV holds the heading to the reference)
+        df['dir'] = (df['Bearing'] - 180) % 360
+
+        # Calculate the x coordinates from the distance and heading
+        df['x'] = (df['dir'].apply(
+            math.radians).apply(math.sin) * df['h']).astype(int)
+
+        # Same for y, which is inverted in order to be read correctly
+        # by the Qt coordinate system
+        df['y'] = (df['dir'].apply(
+            math.radians).apply(math.cos) * -1 * df['h']).astype(int)
+
+        # Convert everything to a single list of markers in tuple form
+        x = df['x'].tolist()
+        y = df['y'].tolist()
+        marker = df['Marker'].tolist()
+        label = df['Name'].tolist()
+        depth = df['Depth'].tolist()
+        done = df['Done'].tolist()
+        self._markers = zip(x, y, marker, label, depth, done)
+
+        self._extents_x = (df['x'].min(), df['x'].max())
+        self._extents_y = (df['y'].min(), df['y'].max())
+
+    def get_markers(self):
+        return self._markers
+
+    def get_extents(self):
+        return (self._extents_x, self._extents_y)
